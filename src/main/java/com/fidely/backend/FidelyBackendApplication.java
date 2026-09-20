7187ddb50580404a23419cfd818a840fc7732964
iliesabdelsadok <ilies.abdelsadok@gmail.com>
2026-09-20 11:04:35 +0200

    first commit

---
+++ src/main/java/com/fidely/backend/FidelyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fidely.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FidelyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fidely/backend/FidelyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidely.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
